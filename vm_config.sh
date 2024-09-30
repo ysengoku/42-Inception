91f4641de7abc1e1c909affe55d956ab06888e7a
yusengok <ysengoku.tourisme@gmail.com>
2024-09-30 11:42:52 +0200

    Update vm_config

--- vm_config.sh
+++ vm_config.sh
@@ -7,6 +7,7 @@ sudo apt-get upgrade -y
 # Install necessary packages
 sudo apt install git
 sudo apt install make
+sudo apt install vim
 
 # Add Docker's official GPG key
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
