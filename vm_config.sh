77c2e37c340521b4554b2133c9033a0769c2d54c
yusengok <yusengok@student.42lyon.fr>
2024-09-30 11:38:50 +0200

    Update

--- vm_config.sh
+++ vm_config.sh
@@ -7,19 +7,16 @@ sudo apt-get upgrade -y
 # Install necessary packages
 sudo apt install make
 sudo apt install git
+sudo apt install vim
 
 # Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # Set up the stable repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 # Install Docker Engine
 sudo apt-get install docker-ce -y
-sudo apt-get update
-
-# Install Docker Compose
-sudo apt-get install docker-compose -y
 sudo apt-get update
 
 # Clean up
