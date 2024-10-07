8df5d8ef05f6da4f42383877991ec8c28d69abb2
yusengok <yusengok@student.42lyon.fr>
2024-10-07 10:19:05 +0200

    Update vm_config

--- vm_config.sh
+++ vm_config.sh
@@ -14,6 +14,10 @@ sudo apt install git make vim
 sudo apt-get update
 
 # Install Docker Engine
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get install docker-ce -y
 sudo apt-get update
 sudo systemctl enable docker
