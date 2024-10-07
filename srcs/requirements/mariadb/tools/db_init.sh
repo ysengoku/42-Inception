b349987b5d0a6839c2a7edb87ea5c45d37c8c6f6
yusengok <yusengok@student.42.fr>
2024-10-07 13:22:14 +0200

    Update mariadb config

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -2,12 +2,15 @@
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '$(SQL_DATABASE)'";
-mysql -e "CREATE USER IF NOT EXISTS '$(SQL_USER)'@'localhost' IDENTIFIED BY '$(SQL_PASSWORD)'";
-mysql -e "GRANT ALL PRIVILEGES ON '$(SQL_DATABASE)' . * TO '$(SQL_USER)'@'%' IDENTIFIED BY '$(SQL_PASSWORD)'";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(SQL_ROOT_PASSWORD)'";
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}' . * TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES";
+
+
+mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown;
 exec mysqld_safe;
