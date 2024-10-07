9f4b1742d5e47c84765f3761c21a1e3b7a52a7fa
yusengok <ysengoku.tourisme@gmail.com>
2024-10-07 15:13:22 +0200

    Add wp config

---
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#mv wp-cli.phar /usr/local/bin/wp
+
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	wp config create --allow-root \
+				--dbname=$SQL_DATABASE \
+				--dbuser=$SQL_USER \
+				--dbpass=$SQL_PASSWORD \
+				--dbhost=mariadb:3306 \
+				--path='/var/www/wordpress'
+
+wp core install --url=$DOMAIN_NAME \
+				--title=$SITE_TITLE \
+				--admin_user=$WP_ADMIN \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_EMAIL \
+				--skip-email
+
+wp user create $WP_USER $WP_ADMIN_EMAIL \
+				--role=author \
+				--user_pass=$WP_USER_PASSWORD \
+				--porcelain
