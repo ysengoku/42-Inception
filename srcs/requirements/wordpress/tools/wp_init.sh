54707e4a3d8efb7d54e1fad55b4d3f6ac115049a
ysengoku <ysengoku.tourisme@gmail.com>
2024-10-09 20:38:24 +0200

    Update docker compose for mariadb & update Dockerfile for wordpress

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -4,26 +4,32 @@
 #chmod +x wp-cli.phar
 #mv wp-cli.phar /usr/local/bin/wp
 
+sleep 10
+
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	wp core download --allow-root \
+					--path='/var/www/html'
+
 	wp config create --allow-root \
-				--dbname=$SQL_DATABASE \
-				--dbuser=$SQL_USER \
-				--dbpass=$SQL_PASSWORD \
-				--dbhost=mariadb:3306 \
-				--path='/var/www/wordpress'
+					--dbname=${SQL_DATABASE} \
+					--dbuser=${SQL_USER} \
+					--dbpass=${SQL_PASSWORD} \
+					--dbhost=mariadb:3306 \
+					--path='/var/www/html'
+
+	wp core install --url=${DOMAIN_NAME} \
+					--title=${SITE_TITLE} \
+					--admin_user=${WP_ADMIN} \
+					--admin_password=${WP_ADMIN_PASSWORD} \
+					--admin_email=${WP_ADMIN_EMAIL} \
+					--skip-email \
+					--path='/var/www/html'
+
+	wp user create --role=author ${WP_USER} ${WP_USER_EMAIL}\
+					--user_pass=${WP_USER_PASSWORD} \
+					--porcelain \
+					--path='/var/www/html'
 fi
-
-wp core install --url=$DOMAIN_NAME \
-				--title=$SITE_TITLE \
-				--admin_user=$WP_ADMIN \
-				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--skip-email
-
-wp user create $WP_USER $WP_ADMIN_EMAIL \
-				--role=author \
-				--user_pass=$WP_USER_PASSWORD \
-				--porcelain
 
 mkdir -p /run/php
 chown www-data:www-data /run/php
