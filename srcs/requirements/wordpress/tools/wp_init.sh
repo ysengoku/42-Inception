57f18af6c61e04dea82f45e4555200ba8e86218a
yusengok <yusengok@student.42lyon.fr>
2024-10-10 12:15:03 +0200

    Issue fix on going in mariadb user creating

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#chmod +x wp-cli.phar
-#mv wp-cli.phar /usr/local/bin/wp
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
 
 sleep 10
 
@@ -11,22 +11,23 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 					--path='/var/www/html'
 
 	wp config create --allow-root \
-					--dbname=${SQL_DATABASE} \
-					--dbuser=${SQL_USER} \
-					--dbpass=${SQL_PASSWORD} \
-					--dbhost=mariadb:3306 \
+					--dbname='${SQL_DATABASE}' \
+					--dbuser='${SQL_USER}' \
+					--dbpass='${SQL_PASSWORD}' \
+					--dbhost='mariadb:3306' \
 					--path='/var/www/html'
 
-	wp core install --url=${DOMAIN_NAME} \
-					--title=${SITE_TITLE} \
-					--admin_user=${WP_ADMIN} \
-					--admin_password=${WP_ADMIN_PASSWORD} \
-					--admin_email=${WP_ADMIN_EMAIL} \
+	wp core install --allow-root \
+					--url='${DOMAIN_NAME}' \
+					--title='${SITE_TITLE}' \
+					--admin_user='${WP_ADMIN}' \
+					--admin_password='${WP_ADMIN_PASSWORD}' \
+					--admin_email='${WP_ADMIN_EMAIL}' \
 					--skip-email \
 					--path='/var/www/html'
 
-	wp user create --role=author ${WP_USER} ${WP_USER_EMAIL}\
-					--user_pass=${WP_USER_PASSWORD} \
+	wp user create --role=author '${WP_USER}' '${WP_USER_EMAIL}'\
+					--user_pass='${WP_USER_PASSWORD}' \
 					--porcelain \
 					--path='/var/www/html'
 fi
