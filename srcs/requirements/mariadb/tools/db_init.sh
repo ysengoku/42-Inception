3319e4816c41a317d6be82b960ca967062c5bf3d
ysengoku <ysengoku.tourisme@gmail.com>
2024-10-10 21:51:46 +0200

    Update mariadb config

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-# Start the database on background
-#mysqld --datadir='/var/lib/mysql'
-service mariadb start
-
 # Check if the required environment variables are set
 if  [ -z "$SQL_ROOT_PASSWORD" ] || [ -z "$SQL_USER" ] || [ -z "$SQL_PASSWORD" ] || [ -z "$SQL_DATABASE" ]; then
 	echo "Missing environment variables";
 	exit 1;
 fi
 
+# Initialize the MySQL data directory and create the system tables
+# --user : The user that will own the data directory
+# --datadir : The directory where the data will be stored
+mysqld_install_db --user=mysql --datadir=/var/lib/mysql
+
+mkdir -p /run/mysql
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+# Start the MySQL server in background (& : as a daemon)
+mysqld --user=mysql --datadir=/var/lib/mysql &
+# Get the pid of the MySQL server
+pid=$!
+
 # Wait for the database to start
-DB_HOST="${DB_HOST:-localhost}"
-until mysqladmin ping -h"${DB_HOST}" --silent; do
+until mysqladmin -u root -p${SQL_ROOT_PASSWORD} >/dev/null 2>&1; do
 	echo "Waiting for MariaDB to start...";
 	sleep 1;
 done
 
-# Set the root password
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Create the database and user
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\` .* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}';"
 # Activate the changes
-mysql -e "FLUSH PRIVILEGES";
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+# Set the root password
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Shutdown the database and restart it on foreground
-mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown;
+kill "$pid"
+wait "$pid"
+exec mysqld --user=mysql
--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p /var/www/html && cd /var/www/html
+wget https://wordpress.org/wordpress-6.6.2.tar.gz && \
+	tar -xzf wordpress-6.6.2.tar.gz && \
+	rm wordpress-6.6.2.tar.gz && \
+	chmod 755 /var/www/html/wordpress && chown -R www-data:www-data /var/www/html/wordpress
+
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -7,24 +13,27 @@ mv wp-cli.phar /usr/local/bin/wp
 sleep 10
 
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-        wp core download --allow-root \
-            --path='/var/www/html'
+        wp core download --allow-root
 
-        wp config create --allow-root \
+        wp core config \
             --dbname="${SQL_DATABASE}" \
             --dbuser="${SQL_USER}" \
             --dbpass="${SQL_PASSWORD}" \
             --dbhost='mariadb:3306' \
-            --path='/var/www/html'
+            --allow-root
 
-        wp core install --allow-root \
+        wp core install \
             --url="${DOMAIN_NAME}" \
             --title="${SITE_TITLE}" \
             --admin_user="${WP_ADMIN}" \
             --admin_password="${WP_ADMIN_PASSWORD}" \
             --admin_email="${WP_ADMIN_EMAIL}" \
-            --skip-email \
-            --path
+            --allow-root 
+
+        wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
+            --role=author \
+            --user_pass="${WP_USER_PASSWORD}" \
+            --allow-root
 fi
 
 mkdir -p /run/php
