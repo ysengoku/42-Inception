e364732d25278bd5f9a7c7b16901bfe4a9ea3b9f
yusengok <yusengok@student.42lyon.fr>
2024-10-10 14:25:09 +0200

    Fix wordpress user creation

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -7,30 +7,24 @@ mv wp-cli.phar /usr/local/bin/wp
 sleep 10
 
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-	wp core download --allow-root \
-					--path='/var/www/html'
+        wp core download --allow-root \
+            --path='/var/www/html'
 
-	wp config create --allow-root \
-					--dbname='${SQL_DATABASE}' \
-					--dbuser='${SQL_USER}' \
-					--dbpass='${SQL_PASSWORD}' \
-					--dbhost='mariadb:3306' \
-					--path='/var/www/html'
+        wp config create --allow-root \
+            --dbname="${SQL_DATABASE}" \
+            --dbuser="${SQL_USER}" \
+            --dbpass="${SQL_PASSWORD}" \
+            --dbhost='mariadb:3306' \
+            --path='/var/www/html'
 
-	wp core install --allow-root \
-					--url='${DOMAIN_NAME}' \
-					--title='${SITE_TITLE}' \
-					--admin_user='${WP_ADMIN}' \
-					--admin_password='${WP_ADMIN_PASSWORD}' \
-					--admin_email='${WP_ADMIN_EMAIL}' \
-					--skip-email \
-					--path='/var/www/html'
-
-	wp user create --role=author '${WP_USER}' '${WP_USER_EMAIL}'\
-					--user_pass='${WP_USER_PASSWORD}' \
-					--porcelain \
-					--path='/var/www/html'
-fi
+        wp core install --allow-root \
+            --url="${DOMAIN_NAME}" \
+            --title="${SITE_TITLE}" \
+            --admin_user="${WP_ADMIN}" \
+            --admin_password="${WP_ADMIN_PASSWORD}" \
+            --admin_email="${WP_ADMIN_EMAIL}" \
+            --skip-email \
+            --path
 
 mkdir -p /run/php
 chown www-data:www-data /run/php
