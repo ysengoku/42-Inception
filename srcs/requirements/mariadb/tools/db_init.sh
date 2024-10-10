7003f47858347749cf8434f0d66d7a4f9c77f8f7
yusengok <yusengok@student.42lyon.fr>
2024-10-10 14:46:09 +0200

    Issue fix in mariadb user creation on going

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Start the database on background
-#service mysql start;
-#exec mysqld --skip-networking --datadir='/var/lib/mysql'
+#mysqld --datadir='/var/lib/mysql'
 
 # Check if the required environment variables are set
 if  [ -z "$SQL_ROOT_PASSWORD" ] || [ -z "$SQL_USER" ] || [ -z "$SQL_PASSWORD" ] || [ -z "$SQL_DATABASE" ]; then
@@ -21,12 +20,12 @@ done
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Create the database and user
-mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-mysql -u root -p"$SQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -p"$SQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}' .* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON '${SQL_DATABASE}' .* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
 # Activate the changes
 mysql -e "FLUSH PRIVILEGES";
 
 # Shutdown the database and restart it on foreground
 mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown;
-#exec mysqld_safe;
