676781df4a9ddb20267a0129e79c3fc4ea5c9d91
yusengok <yusengok@student.42.fr>
2024-10-11 09:30:53 +0200

    Update to test

--- srcs/requirements/mariadb/tools/db_init.sh
+++ srcs/requirements/mariadb/tools/db_init.sh
@@ -28,7 +28,6 @@ until mysqladmin ping -u root -p${SQL_ROOT_PASSWORD} >/dev/null 2>&1; do
 	sleep 1;
 done
 
-
 # Create the database and user
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${SQL_USER}' IDENTIFIED BY '${SQL_PASSWORD}';"
