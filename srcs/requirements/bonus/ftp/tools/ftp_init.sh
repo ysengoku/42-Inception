9000abe9f04df6c1feddde58ef9a307f99d372f8
ysengoku <ysengoku.tourisme@gmail.com>
2024-10-17 20:05:12 +0200

    Update ftp port config

--- srcs/requirements/bonus/ftp/tools/ftp_init.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_init.sh
@@ -4,19 +4,20 @@ service vsftpd start
 
 if [ -n "{FTP_USER}" ] && [ -n "{FTP_PASSWORD}" ]; then
 
+	# Create a user and set a password
 	adduser -disabled-password --gecos "" ${FTP_USER}
-
 	echo "${FTP_USER}:${FTP_PASSWORD}" | /usr/sbin/chpasswd &> /dev/null
 	echo "${FTP_USER}" | tee -a /etc/vsftpd.userlist &> /dev/null
 
+	# Create a directory for the user
 	mkdir -p /home/${FTP_USER}/ftp
 	chown nobody:nogroup /home/${FTP_USER}/ftp
 	chmod a-w /home/${FTP_USER}/ftp
-
 	mkdir /home/${FTP_USER}/ftp/files
 	chown ${FTP_USER}:${FTP_USER} /home/${FTP_USER}/ftp/files
 fi
 
+# Create a directory for the anonymous user
 mkdir -p /var/ftp/pub
 chown ftp:ftp /var/ftp/pub
 
